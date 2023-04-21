bd419a7f8e8dff35f0021a0bdf04aec7210eea8b
PierreWTH <wietrichpierre@gmail.com>
2023-04-21 15:56:41 +0200

    ajout fonction getPokemonData

--- apiPokemon/js/script.js
+++ apiPokemon/js/script.js
@@ -1,3 +1,4 @@
+// Affiche les pokémons lors du clic
 const form = document.querySelector("form")
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -5,20 +6,29 @@ form.addEventListener("submit", (event) => {
     getInfosAPI(inputNbPokemon);
 })
 
+// Définir le nombre de pokémon que l'on veut afficher
 function getInfosAPI(inputNbPokemon){
     const pokemonDiv = document.querySelector(".pokemonDiv")
     
     fetch( "https://pokeapi.co/api/v2/pokemon/?limit="+inputNbPokemon.value )  
     .then((response) => response.json())
     .then((data) => {
-        console.log(data.results)
         data.results.forEach((pokemon) => {
-        pokemonDiv.innerHTML += "<p>" + pokemon.name + "</p>"
-        pokemonDiv.innerHTML += "<img src =https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+pokemon.XXX+"</img>" 
-        
+        getPokemonData(pokemon)
     })
 
     })
 
 .catch((error) => pokemonDiv.innerHTML = error)
+}
+
+// Récuperer les infos de chaque pokémon
+function getPokemonData(pokemon){
+    let url = pokemon.url
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(function(pokeData){
+        console.log(pokeData)
+    })
 }
