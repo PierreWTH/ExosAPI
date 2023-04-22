7e21c872ed7f8fdc1ce133fa7b89a33d691e3849
PierreWTH <wietrichpierre@gmail.com>
2023-04-22 11:43:40 +0200

    affichage nom, id, img, type ok

--- apiPokemon/js/script.js
+++ apiPokemon/js/script.js
@@ -3,17 +3,18 @@ const form = document.querySelector("form")
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const inputNbPokemon = document.querySelector(".nbPokemon")
-    getInfosAPI(inputNbPokemon);
+    getPokemon(inputNbPokemon);
 })
 
 // Définir le nombre de pokémon que l'on veut afficher
-function getInfosAPI(inputNbPokemon){
+function getPokemon(inputNbPokemon){
     const pokemonDiv = document.querySelector(".pokemonDiv")
     
     fetch( "https://pokeapi.co/api/v2/pokemon/?limit="+inputNbPokemon.value )  
     .then((response) => response.json())
-    .then((data) => {
-        data.results.forEach((pokemon) => {
+    .then((allPokemon) => {
+        console.log(allPokemon)
+        allPokemon.results.forEach(function(pokemon) {
         getPokemonData(pokemon)
     })
 
@@ -30,5 +31,33 @@ function getPokemonData(pokemon){
     .then(response => response.json())
     .then(function(pokeData){
         console.log(pokeData)
+        printPokemon(pokeData)
+
     })
+}
+
+// Afficher les donneés
+
+function printPokemon(pokeData){
+    let allPokemonContainer = document.getElementById('allPokemonDiv')
+    let pokemonContainer = document.createElement("div")
+    
+    // Afficher le nom
+    let pokemonName = document.createElement('h4')
+    pokemonName.innerText = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1);
+
+    // Afficher le numéro
+    let pokemonNumber = document.createElement('p')
+    pokemonNumber.innerText = "#" + pokeData.id.toString().padStart(3, '0');
+
+    // Afficher l'image
+    let pokemonImg = document.createElement('img')
+    pokemonImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+pokeData.id+'.png'
+
+    // Afficher le type
+    let pokemonType = document.createElement('p')
+    pokemonType.innerText = pokeData.types[0].type.name.charAt(0).toUpperCase() + pokeData.types[0].type.name.slice(1)
+    
+    allPokemonContainer.append(pokemonName, pokemonNumber, pokemonImg, pokemonType);
+    allPokemonContainer.appendChild(pokemonContainer);
 }
